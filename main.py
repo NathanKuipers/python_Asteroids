02469b71605e75b215a7629ae8ab276cdadefa47
NathanKuipers <nathankuipers123@gmail.com>
2026-02-15 18:33:15 +1100

    create player on screen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from turtle import Screen
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
@@ -10,14 +10,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             screen.fill("black")
+            player.draw(screen)
+            player.update(dt)
             pygame.display.flip()
-
+            
+        clock.tick(60)  # Limit to 60 FPS
+        dt = clock.get_time() / 1000.0  # Convert milliseconds to seconds
+        
 
 if __name__ == "__main__":
     main()
